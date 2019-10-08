81fbc03ce4b8b606b83ffbf441096cedf97fe88b
makotosvetlapali <donpedjas@gmail.com>
2019-10-08 23:12:10 +0200

    Done

--- src/MakeMeRich.java
+++ src/MakeMeRich.java
@@ -1,6 +1,11 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+
 
 public class MakeMeRich {
 	public static final List<String> symbols = Arrays.asList("AMD", "HPQ",
@@ -9,15 +14,17 @@ public class MakeMeRich {
 			"YHOO");
 
 	public static void main(String[] args) {
+		
+		symbols.stream().forEach((Each) ->
+				System.out.print(Each + ", "));
+		symbols.stream().forEach((Each) ->
+				System.out.println(StockUtil.getPrice(Each)));
 
-		// 1. Print these symbols using a Java 8 for-each and lambdas
-
-		// 2. Use the StockUtil class to print the price of Bitcoin
-
-		// 3. Create a new List of StockInfo that includes the stock price
-	
-		// 4. Find the highest-priced stock under $500
-	
+				List<StockInfo> StockP = new ArrayList<StockInfo>();
+				for (String str1ng : symbols) {
+					StockP.add(StockUtil.getPrice(str1ng));}
+				System.out.println(StockP);
+		
 	}
 
 }
